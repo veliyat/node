b4c18a00aec15f64e8d8122a3f64a724fa166db0
Jay Shah <jpatel@agileinfoways.com>
2015-01-19 12:27:04 +0530

    sent users in json format on users route, added proper comments

--- express/sampleApp/routes/users.js
+++ express/sampleApp/routes/users.js
@@ -2,13 +2,28 @@ var express = require('express');
 var router = express.Router();
 var config = require('../config');
 var mongoose = require('mongoose');
-mongoose.model('users',{name:String});
-/* GET users listing. */
-router.get('/', function(req, res) {
-  // res.render('users', { title: 'Users Page',host:config.host });
-  mongoose.model('users').find(function(err,users){
-  	console.log(users);
-  	res.render('users', { title: 'Users Page',host:config.host,users: users });
+
+/**
+ * Creating `users` collection in mongo db if not exists.
+ */
+mongoose.model('users', {
+							name:String
+						}
+			);
+
+/**
+ * Send users stored in `users` collection in default `json` format
+ * @param  {object} req  [Request Object]
+ * @param  mixed res [Response Object]
+ * @return mixed      [Mixed Response - Can be anything html, xml, json or any other format]
+ */
+router.get('/', function (req, res) {
+
+  mongoose.model('users').find(function (err, users) {
+  	
+  	// res.render('users', { title: 'Users Page',host:config.host,users: users });
+  	res.send(users); // Send all data in default format `json` 
+
   });
 });
 
