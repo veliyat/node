7cc5576165ce18ffade0b203778283653c29fd72
unknown <harsh.doshi@agileinfoways.com>
2015-01-21 10:12:25 +0530

    Added sorting method

--- express/sampleApp/routes/users.js
+++ express/sampleApp/routes/users.js
@@ -18,12 +18,37 @@ mongoose.model('users', {
  * @return mixed      [Mixed Response - Can be anything html, xml, json or any other format]
  */
 router.get('/', function (req, res) {
+	res.render('users', { title: 'Users Page',host:config.host});
+});
+
+router.get('/all', function (req, res) {
 
   mongoose.model('users').find(function (err, users) {
   	
   	res.render('users', { title: 'Users Page',host:config.host,users: users });
   	// res.send(users); // Send all data in default format `json` 
 
+  });
+});
+
+router.get('/asc', function (req, res) {
+
+  var users = mongoose.model('users').find();
+  users.sort('name'); // Sort by name ascending
+  users.exec(function (err, users){
+  	res.render('users', { title: 'Users Page',host:config.host,users: users });
+  	// res.send(users);
+  });
+
+});
+
+router.get('/desc', function (req, res) {
+
+  var users = mongoose.model('users').find();
+  users.sort('-name'); // Sort by name descending
+  users.exec(function (err, users){
+  	res.render('users', { title: 'Users Page',host:config.host,users: users });
+  	// res.send(users);
   });
 });
 
